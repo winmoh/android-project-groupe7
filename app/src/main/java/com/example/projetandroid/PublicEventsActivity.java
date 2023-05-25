34848e5b00ec35dfa0242ffca9ea68d4498caffa
winmoh <103373835+winmoh@users.noreply.github.com>
2023-05-25 01:14:03 +0100

    Update PublicEventsActivity.java

--- app/src/main/java/com/example/projetandroid/PublicEventsActivity.java
+++ app/src/main/java/com/example/projetandroid/PublicEventsActivity.java
@@ -5,6 +5,8 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,29 +14,41 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 
 public class PublicEventsActivity extends AppCompatActivity {
 
     String username;
-
+    String key = "";
+    String imageUrl ;
     ListView publicEvents;
 
     FirebaseDatabase database;
 
     DatabaseReference eventsRef,eventAttendeeRef;
+    FirebaseStorage storage=FirebaseStorage.getInstance();
+    StorageReference sref;
 
 
     ArrayList<EventItem> public_events = new ArrayList<>();
@@ -65,8 +79,7 @@ public class PublicEventsActivity extends AppCompatActivity {
                 }
                 EventItem currentEvent = public_events.get(position);
 
-                TextView clubName_p = convertView.findViewById(R.id.clubName_p);
-                clubName_p.setText(currentEvent.getClub());
+
 
 
                 TextView eventItemTitle_p = convertView.findViewById(R.id.eventItemTitle_p);
@@ -83,6 +96,33 @@ public class PublicEventsActivity extends AppCompatActivity {
 
                 TextView eventItemTime_p = convertView.findViewById(R.id.eventItemTime_p);
                 eventItemTime_p.setText(currentEvent.getTime());
+
+                imageUrl=currentEvent.getEventImage();
+
+                ImageView eventItemImage_p=convertView.findViewById(R.id.eventImage_p);
+                storage = FirebaseStorage.getInstance();
+
+                sref=storage.getReferenceFromUrl(imageUrl);
+                try {
+                    File localFile = File.createTempFile("imageHolder", ".jpg");
+                    sref.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                            Bitmap bitmap= BitmapFactory.decodeFile(localFile.getAbsolutePath());
+                            eventItemImage_p.setImageBitmap(bitmap);
+
+
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(PublicEventsActivity.this,e.getMessage(),Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+                catch(IOException e){
+                    e.setStackTrace(e.getStackTrace());
+                }
 
                 convertView.findViewById(R.id.eventItemAttend_p).setOnClickListener(new View.OnClickListener() {
                     @Override
